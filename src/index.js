16e7470dd9bb8bc6393e1335c461398895e84ea7
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-10-13 10:00:44 +0300

    feat: add reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let reverse_n = 0;
+    while ( n !== 0) {
+        reverse_n = (reverse_n * 10) + (n % 10); 
+        n = Math.floor( n / 10);
+    }
+    return reverse_n;
 }
