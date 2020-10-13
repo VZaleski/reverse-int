46badb67d13965ad784ae7ec85dab1a3b3590e32
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-10-13 11:56:10 +0300

    fix: reverse_number

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = function reverse (n) {
     let reverse_n = 0;
     while ( n !== 0) {
+        n = Math.abs(n);
         reverse_n = (reverse_n * 10) + (n % 10); 
         n = Math.floor( n / 10);
     }
